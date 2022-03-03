447d2cfb4d974983b3c64d88e7e337883523a4a1
Kelly Barbosa <kyba@gft.com>
2022-03-03 16:10:06 -0300

    versão 0.0.1

---
+++ Arrays.cs
@@ -0,0 +1,16 @@
+﻿namespace Arrays
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var myArray = new int[5] {1, 2, 3, 4, 5};
+            myArray[0] = 12;
+
+            foreach (var item in myArray)
+                Console.WriteLine(item);
+        }
+    }
+}
